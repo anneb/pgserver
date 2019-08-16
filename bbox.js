3b5cf385d5de9a67814b6de5e9e03e6906c4da47
Anne Blankert <anne.blankert@geodan.nl>
2019-08-16 13:15:59 +0200

    cache bbox,translate to english,support views

--- bbox.js
+++ bbox.js
@@ -1,6 +1,8 @@
 // based on https://raw.githubusercontent.com/tobinbradley/dirt-simple-postgis-http-api/master/routes/bbox.js
 
 const sqlTableName = require('./utils/sqltablename.js');
+const DirCache = require('./utils/dircache.js')
+const cache = new DirCache('./cache');
 
 
 const sql = (params, query) => {
@@ -16,6 +18,30 @@ with srid as
 (select st_extent(st_transform(st_setsrid(st_envelope(bboxll),4326),srid)) bboxsrid from bboxll,srid)
 select allrows, geomrows, bboxll,srid,bboxsrid from bboxll,srid,bboxsrid
       `
+}
+
+let cacheMiddleware = async(req, res, next) => {
+  const cacheDir = `${req.params.table}/bbox`
+  const key = ((req.query.geom_column?req.query.geom_column:'geom') 
+    + (req.query.columns?'_'+req.query.columns:'')
+    + (req.query.filter?'_'+req.query.filter:''))
+    .replace(/[\W]+/g, '_');
+
+  const bbox = await cache.getCachedFile(cacheDir, key);
+  if (bbox) {
+    console.log(`bbox cache hit for ${cacheDir}?${key}`);
+    res.header('Content-Type', 'application/json').send(bbox);
+    return;
+  } else {
+    res.sendResponse = res.send;
+    res.send = (body) => {
+      if (res.statusCode >= 200 && res.statusCode < 300) {
+        cache.setCachedFile(cacheDir, key, body);
+      }
+      res.sendResponse(body);
+    }
+    next();
+  }
 }
 
 module.exports = function(app, pool) {
@@ -38,11 +64,6 @@ module.exports = function(app, pool) {
  *         description: name of geometry column (default 'geom')
  *         in: query
  *         required: false
- *       - name: strid
- *         description: 'The SRID for the returned centroid. The default is <em>4326</em> WGS84 Lat/Lng.'
- *         in: query
- *         required: false
- *         type: integer
  *       - name: filter
  *         type: string
  *         description: 'Optional filter parameters for a SQL WHERE statement.'
@@ -50,13 +71,47 @@ module.exports = function(app, pool) {
  *         required: false
  *     responses:
  *       200:
- *         description: vector tile
- *       204:
- *         description: no data (empty tile)
+ *         description: boundingboxes and row counters
+ *         content:
+ *            application/json
+ *         schema:
+ *            type: object
+ *            properties:
+ *               allrows:
+ *                 type: integer
+ *                 description: number of rows in table
+ *               geomrows:
+ *                  type: integer
+ *                  description: number of non-null geometries in table
+ *               bboxll:
+ *                  description: boundingbox of geometries defined in latitude and longitude
+ *                  type: array
+ *                  minItems: 2
+ *                  maxItems: 2
+ *                  items:
+ *                     type: array
+ *                     minItems: 2
+ *                     maxItems: 2
+ *                     items: 
+ *                       type: number
+ *               srid:
+ *                   type: integer
+ *                   description: id of spatial reference system (EPSG code) used by geometries
+ *               bboxsrid:
+ *                  description: boundingbox of geometries defined using geometry srid 
+ *                  type: array
+ *                  minItems: 2
+ *                  maxItems: 2
+ *                  items:
+ *                     type: array
+ *                     minItems: 2
+ *                     maxItems: 2
+ *                     items: 
+ *                       type: number
  *       422:
  *         description: invalid datasource or columnname
  */
-  app.get('/api/bbox/:table', async (req, res)=>{
+  app.get('/api/bbox/:table', cacheMiddleware, async (req, res)=>{
       if (!req.query.geom_column) {
         req.query.geom_column = 'geom'; //default
       }
